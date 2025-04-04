ce2691f6f6b4db269d457993ee274e9badcbec3b
LucianoBuhler <lucianobuhler@gmail.com>
2025-04-04 15:26:32 -0300

    Connected the Signup with BE; added util/errors file

--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -1,35 +1,45 @@
+"use client"
+
 import { Button, Link, Stack, TextField } from "@mui/material";
 import NextLink from "next/link";
+import { useFormState } from "react-dom";
+import createUser from "./create-user";
 
 export default function Signup() {
+  const [state, formAction] = useFormState(createUser, {error: ""})
+
   return (
-    <Stack spacing={2} className="w-full max-w-xs">
-      <h1>Signup</h1>
-      <p>Signup page</p>
-      <TextField
-        label="Email"
-        variant="outlined"
-        type="email"
-        required
-        autoComplete="email"
-        autoFocus
-        margin="normal" />
-      <TextField
-        label="Password"
-        variant="outlined"
-        type="password"
-        required
-        autoComplete="current-password"
-        margin="normal" />
-      <Button
-        variant="contained"
-        color="primary"
-        type="submit">
-          Signup
-      </Button>
-      <Link component={NextLink} href="/auth/login" className="self-center"> 
-        Login
-      </Link>
-    </Stack>
+    <form action={formAction} className="w-full max-w-xs">
+      <Stack spacing={2}>
+        <h1>Signup</h1>
+        <p>Signup page</p>
+        <TextField
+          label="Email"
+          name="email"
+          variant="outlined"
+          type="email"
+          required
+          autoFocus 
+          helperText={state.error}
+          error={!!state.error}/>
+        <TextField
+          label="Password"
+          name="password"
+          variant="outlined"
+          type="password"
+          required
+          helperText={state.error}
+          error={!!state.error}/>
+        <Button
+          variant="contained"
+          color="primary"
+          type="submit">
+            Signup
+        </Button>
+        <Link component={NextLink} href="/auth/login" className="self-center"> 
+          Login
+        </Link>
+      </Stack>
+    </form>
   )
 }
