6e35cd99ade96c1c56edd4497e0c1eb5de328329
LucianoBuhler <lucianobuhler@gmail.com>
2025-04-08 00:58:48 -0300

    Fixed authentication issue: the setAuthCookie was not working as expected, set as unauthenticated after successfull login

--- app/auth/login/login.ts
+++ app/auth/login/login.ts
@@ -1,20 +1,18 @@
 "use server" ;
 
-import { FormError } from "@/app/common/interfaces/form-error.interface";
-import { redirect } from "next/navigation";
-import { getErrorMessage } from "@/app/common/util/errors";
-import { API_URL } from "@/app/common/constants/api";
-import { cookies } from "next/headers";
-// import { post } from "@/app/util/fetch";
 import { jwtDecode } from "jwt-decode";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+import { API_URL } from "@/app/common/constants/api";
+import { getErrorMessage } from "@/app/common/util/errors";
+import { FormError } from "@/app/common/interfaces/form-error.interface";
 import { AUTHENTICATION_COOKIE } from "../auth-cookie";
 
 export default async function login(_prevState: FormError, formData: FormData) {
-  // const { error } = await post('auth/login', formData);
   const res = await fetch(`${API_URL}/auth/login`, {
       method: "POST",
       headers: {
-        "Content-Type": "application/json",},
+        "Content-Type": "application/json" },
       body: JSON.stringify(Object.fromEntries(formData)),
     });
     const parsedRes = await res.json(); 
@@ -29,8 +27,7 @@ const setAuthCookie = async (response: Response) => {
   const setCookieHeader = response.headers.get("Set-Cookie");
   if (setCookieHeader) {
     const token = setCookieHeader.split(";")[0].split("=")[1];
-    const cookieStore = await cookies();
-    cookieStore.set({
+    (await cookies()).set({
       name: AUTHENTICATION_COOKIE,
       value: token,
       secure: true,
@@ -38,4 +35,4 @@ const setAuthCookie = async (response: Response) => {
       expires: new Date(jwtDecode(token).exp! * 1000),
     });
   }
-}
+};
